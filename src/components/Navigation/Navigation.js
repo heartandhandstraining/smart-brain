d6ceea2545565369e88edee335d04c4d76c154ba
jiaxuanc <jiaxuan.chen@alumni.ubc.ca>
2018-07-25 15:48:15 -0700

    add signout and background

---
+++ src/components/Navigation/Navigation.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Navigation extends Component {
+	render() {
+		return (
+			<nav style={{display:' flex', justifyContent: 'flex-end'}}>
+				<p> Sign Out </p>
+			</nav>
+		);
+	}
+
+
+}
+
+export default Navigation;
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,18 +1,18 @@
 import React, { Component } from 'react';
 import './App.css';
 import Navigation from '../components/Navigation/Navigation'
-import Logo from '../components/Logo/Logo';
-import ImageLinkForm from '../components/ImageLinkForm/ImageLinkForm';
-import FaceRecognition from '../components/FaceRecognition/FaceRecognition';
+// import Logo from '../components/Logo/Logo';
+// import ImageLinkForm from '../components/ImageLinkForm/ImageLinkForm';
+// import FaceRecognition from '../components/FaceRecognition/FaceRecognition';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
       <Navigation />
-      <Logo />
-      <ImageLinkForm />
-      <FaceRecognition />
+	    {/*<Logo />
+	    <ImageLinkForm />
+	    <FaceRecognition />*/}
 
       </div>
     );
