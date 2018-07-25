24242fa7d92daf934fdcdda32db21ffa9da1e5da
jiaxuanc <jiaxuan.chen@alumni.ubc.ca>
2018-07-25 16:45:28 -0700

    add rank component

--- src/containers/App.js
+++ src/containers/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import Navigation from '../components/Navigation/Navigation'
 import Logo from '../components/Logo/Logo';
 import ImageLinkForm from '../components/ImageLinkForm/ImageLinkForm';
+import Rank from '../components/Rank/Rank';
 // import FaceRecognition from '../components/FaceRecognition/FaceRecognition';
 
 class App extends Component {
@@ -11,6 +12,7 @@ class App extends Component {
       <div className="App">
       <Navigation />
 	    <Logo />
+	    <Rank />
 	    <ImageLinkForm />
 	    {/*<FaceRecognition />*/}
 
