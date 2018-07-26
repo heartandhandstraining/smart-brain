26a795bbe52e05858633d24e5c08b287270ad945
jiaxuanc <jiaxuan.chen@alumni.ubc.ca>
2018-07-25 17:33:15 -0700

    add particles in the background

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,20 +1,27 @@
 import React, { Component } from 'react';
 import './App.css';
+import Particles from 'react-particles-js';
 import Navigation from '../components/Navigation/Navigation'
 import Logo from '../components/Logo/Logo';
 import ImageLinkForm from '../components/ImageLinkForm/ImageLinkForm';
 import Rank from '../components/Rank/Rank';
+import particlesOptions from './ParticlesOptions.json';
 // import FaceRecognition from '../components/FaceRecognition/FaceRecognition';
+
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-      <Navigation />
-	    <Logo />
-	    <Rank />
-	    <ImageLinkForm />
-	    {/*<FaceRecognition />*/}
+      	<Particles className="particles"
+          params={particlesOptions}
+        />
+	      <Navigation />
+		    <Logo />
+		    <Rank />
+		    <ImageLinkForm />
+		    {/*<FaceRecognition />*/}
 
       </div>
     );
